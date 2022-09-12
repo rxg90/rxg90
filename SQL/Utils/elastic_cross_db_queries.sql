67e0b5fd2d25cfc3102b7ba33e5751d11526142e
Rocio Gonzalez Filomeno <90275802+rxg90@users.noreply.github.com>
2022-09-12 11:52:31 -0300

    Create elastic_cross_db_queries.sql

---
+++ SQL/Utils/elastic_cross_db_queries.sql
@@ -0,0 +1,29 @@
+https://stackoverflow.com/questions/6572754/sql-azure-copy-table-between-databases
+
+CREATE MASTER KEY ENCRYPTION BY PASSWORD = '<password>';
+
+CREATE DATABASE SCOPED CREDENTIAL SQL_Credential  
+WITH IDENTITY = '<username>',
+SECRET = '<password>';
+
+CREATE EXTERNAL DATA SOURCE RemoteReferenceData
+WITH
+(
+    TYPE=RDBMS,
+    LOCATION='<server>.database.windows.net',
+    DATABASE_NAME='<db>',
+    CREDENTIAL= SQL_Credential
+);
+
+CREATE EXTERNAL TABLE [dbo].[source_table] (
+    [Id] BIGINT NOT NULL,
+    ...
+)
+WITH
+(
+    DATA_SOURCE = RemoteReferenceData
+)
+
+SELECT *
+ INTO target_table
+FROM source_table
